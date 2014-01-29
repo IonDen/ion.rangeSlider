ecbdee667393fa879a02aa3bfce1ad9e559997b4
Дмитрий Фомин <grandz0d@gmail.com>
2014-01-29 16:52:58 +0400

    Slider by predefined values
    Слайдер из предустановленых значений

--- js/ion.rangeSlider.js
+++ js/ion.rangeSlider.js
@@ -62,6 +62,7 @@
 
             return this.each(function () {
                 var settings = $.extend({
+                    values : [],
                     min: null,
                     max: null,
                     from: null,
@@ -79,8 +80,6 @@
                     onLoad: null,
                     onFinish: null
                 }, options);
-
-
 
                 var slider = $(this),
                     self = this,
@@ -673,10 +672,11 @@
                         toX: 0,
                         toX_pure: 0
                     };
+                    
                     var diapason = settings.max - settings.min;
 
                     if (settings.type === "single") {
-
+	                    
                         nums.fromPers = (nums.fromNumber - settings.min) / diapason * 100;
                         nums.fromX_pure = fullWidth / 100 * nums.fromPers;
                         nums.fromX = Math.round(nums.fromX_pure);
@@ -700,7 +700,6 @@
                         setDiapason();
 
                     }
-
                     numbers = nums;
                     setFields();
                 };
@@ -738,16 +737,21 @@
                         _to, _toW, _toX,
                         _single, _singleW, _singleX,
                         _slW = (sliderWidth / 2);
-
                     if (settings.type === "single") {
 
                         if (!settings.hideText) {
                             $fieldFrom[0].style.display = "none";
                             $fieldTo[0].style.display = "none";
 
-                            _single = settings.prefix +
-                                prettify(numbers.fromNumber) +
-                                settings.postfix;
+                            if(settings.values.length) {
+	                            _single = settings.prefix +
+	                                settings.values[numbers.fromNumber] +
+	                                settings.postfix;
+                            } else {
+	                            _single = settings.prefix +
+	                                prettify(numbers.fromNumber) +
+	                                settings.postfix;
+                            }
                             $fieldSingle.html(_single);
 
                             _singleW = $fieldSingle.outerWidth();
