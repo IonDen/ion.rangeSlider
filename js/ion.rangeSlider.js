8065f5c6db00aa1ae1f4d5765ba22c77a413dc80
Jonathan Marshall <jonathan@globalinbox.co>
2014-09-18 10:02:31 +0200

    Add a function and option shorten grid numbers.
    New option to abbreviate numbers large numbers like '2500000' to '2.5m'
    on the grid.

--- js/ion.rangeSlider.js
+++ js/ion.rangeSlider.js
@@ -90,6 +90,7 @@
                     prettify: true,
                     disable: false,
                     values: null,
+                    abbreviate: false,
                     onLoad: null,
                     onChange: null,
                     onFinish: null
@@ -226,6 +227,9 @@
                 if (slider.data("values")) {
                     settings.values = slider.data("values").split(",");
                 }
+                if (slider.data("abbreviate")) {
+                    settings.abbreviate = slider.data("abbreviate");
+                }
 
 
 
@@ -311,6 +315,23 @@
                     return n;
                 };
 
+                function abbreviateNumber(value) {
+                    if (!testNumber(value)) return value;
+
+                    var newValue = value;
+                    if (value >= 1000) {
+                        var suffixes = ["", "k", "m", "b","t"];
+                        var suffixNum = Math.floor( (""+value).length/3 );
+                        var shortValue = '';
+                        for (var precision = 2; precision >= 1; precision--) {
+                            shortValue = parseFloat( (suffixNum != 0 ? (value / Math.pow(1000,suffixNum) ) : value).toPrecision(precision));
+                            var dotLessShortValue = (shortValue + '').replace(/[^a-zA-Z 0-9]+/g,'');
+                            if (dotLessShortValue.length <= 2) { break; }
+                        }
+                        newValue = shortValue+suffixes[suffixNum];
+                    }
+                    return newValue;
+                }
 
                 var containerHTML = '<span class="irs" id="irs-' + this.plugin_count + '"></span>';
                 slider[0].style.display = "none";
@@ -1120,7 +1141,12 @@
                         } else {
                             text = Math.round(settings.min + ((settings.max - settings.min) / bigNum * i));
                             text = Math.round(text / settings.step) * settings.step;
-                            text = prettify(text);
+                            if (settings.abbreviate) {
+                                text = abbreviateNumber(text);
+                            }
+                            else {
+                                text = prettify(text);
+                            }
                         }
 
                         if (allow_values) {
