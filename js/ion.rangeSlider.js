6e9349ae9304c443c4cd03b10f87324acd869abe
Stefan Hekele <sh@plan2.net>
2016-02-15 13:07:55 +0100

    Update ion.rangeSlider.js for AMD
    Working within requireJS bundles now

--- js/ion.rangeSlider.js
+++ js/ion.rangeSlider.js
@@ -10,7 +10,16 @@
 // http://ionden.com/a/plugins/licence-en.html
 // =====================================================================================================================
 
-;(function ($, document, window, navigator, undefined) {
+(function (factory) {
+    /* jshint strict: false */
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module depending on jQuery.
+        define(['jquery'], factory);
+    } else {
+        // No AMD. Register plugin with global jQuery object.
+        factory(jQuery);
+    }
+}(function ($) {
     "use strict";
 
     // =================================================================================================================
@@ -2304,4 +2313,4 @@
             };
     }());
 
-} (jQuery, document, window, navigator));
+}));
